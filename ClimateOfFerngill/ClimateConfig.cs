97ed44149a00008ee7693a90255870aef30d4567
Nicholas Johnson <koihimenakamura@gmail.com>
2016-10-14 22:14:56 -0700

    - Cleaned up all mentions of MoreRain (the mod this came from) - Added prototype weather to handle seasons - Updated config file for the system. Renamed config file. - Version initial

---
+++ ClimateOfFerngill/ClimateConfig.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using StardewModdingAPI;
+
+namespace ClimatesOfFerngill
+{
+    public class ClimateConfig : Config
+    {
+        public double spgBaseRainChance { get; set; }
+        public double smrBaseRainChance { get; set; }
+        public double falBaseRainChance { get; set; }
+
+        public double spgRainChanceIncrease { get; set; }
+        public double smrRainChanceIncrease { get; set; }
+        public double falRainChanceIncrease { get; set; }
+
+        public double spgBaseWindChance { get; set; }
+        public double smrBaseWindChance { get; set; }
+        public double falBaseWindChance { get; set; }
+        public double winBaseWindChance { get; set; }
+
+        public double spgWindChanceIncrease { get; set; }
+        public double smrWindChanceIncrease { get; set; }
+        public double falWindChanceIncrease { get; set; }
+        public double winWindChanceIncrease { get; set; }
+
+        public double spgBaseStormChance { get; set; }
+        public double smrBaseStormChance { get; set; }
+        public double falBaseStormChance { get; set; }
+
+        public double spgStormChanceIncrease { get; set; }
+        public double smrStormChanceIncrease { get; set; }
+        public double falStormChanceIncrease { get; set; }
+
+        public double spgConvRainToStorm { get; set; }
+        public double smrConvRainToStorm { get; set; }
+        public double falConvRainToStorm { get; set; }
+
+        public double winBaseSnowChance { get; set; }
+        public double winSnowChanceIncrease { get; set; }
+
+        public bool SuppressLog { get; set; }
+
+        public override T GenerateDefaultConfig<T>()
+        {
+            /* Spring Weather:
+             * Starts wet and dries out. Storms should follow this, with the wind being fairly constant (but slackign towards the end)
+             * 
+             * Summer Weather:
+             * Not that windy at start, but it gets progressively more windy. Same as rainy, but it will storm more often.
+             * 
+             * Fall Weather:
+             * Halfway through, we should start monsoon season.  Windy. (Too bad we don't have windy rain!)
+             * 
+             * Winter Weather:
+             * Wet (so snowy), not that windy. No storms in winter. */
+
+            //set defaults for spring weather
+            spgBaseRainChance = .50; 
+            spgRainChanceIncrease = -.0135;
+
+            spgBaseStormChance = .16;
+            spgStormChanceIncrease = -.0055;
+
+            spgBaseWindChance = .22;
+            spgWindChanceIncrease = -.0025;
+
+            spgConvRainToStorm = .0375; //We generally want very few conversions in spring. 
+
+            //set defaults for summer weather
+            smrBaseRainChance = .122;
+            smrRainChanceIncrease = .0065;
+
+            smrBaseStormChance = .006;
+            smrStormChanceIncrease = .0099;
+
+            smrBaseWindChance = .005;
+            smrWindChanceIncrease = .0085;
+
+            smrConvRainToStorm = .5000; //half chance for rain to become storm.
+
+            //set defaults for autumn weather
+            falBaseRainChance = .304;
+            falRainChanceIncrease = .0165;
+
+            falBaseStormChance = .2832;
+            falStormChanceIncrease = .0099;
+
+            falBaseWindChance = .243;
+            falWindChanceIncrease = .0175;
+
+            falConvRainToStorm = .0095;
+
+            //set default for winter weather
+            winBaseWindChance = .65;
+            winWindChanceIncrease = -.012;
+
+            winBaseSnowChance = .766;
+            winSnowChanceIncrease = .005;
+
+            SuppressLog = false;
+            return this as T;
+        }
+    }
+}
--- ClimateOfFerngill/ClimatesOfFerngill.cs
+++ ClimateOfFerngill/ClimatesOfFerngill.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using StardewModdingAPI;
 using StardewValley;
 
@@ -6,12 +7,12 @@ namespace ClimatesOfFerngill
 {
     public class ClimatesOfFerngill : Mod
     {
-        public ModConfig ModConfig { get; private set; } 
+        public ClimateConfig ModConfig { get; private set; } 
         bool gameloaded;
 
         public override void Entry(params object[] objects)
         {
-          ModConfig = new ModConfig().InitializeConfig(BaseConfigPath);
+          ModConfig = new ClimateConfig().InitializeConfig(BaseConfigPath);
           StardewModdingAPI.Events.PlayerEvents.LoadedGame += PlayerEvents_LoadedGame;
           StardewModdingAPI.Events.TimeEvents.DayOfMonthChanged += TimeEvents_DayOfMonthChanged;
           Log.SyncColour("Climates of Ferngill rev20161014 by KoihimeNakamura but based off of Alpha_Omegasis's More Rain", ConsoleColor.Cyan);
@@ -34,18 +35,131 @@ namespace ClimatesOfFerngill
             UpdateWeather();
         }
 
+
         void UpdateWeather(){
-            //sanity check - not entirely neccesary. 
+            //sanity check - wedding
             if (Game1.weatherForTomorrow == Game1.weather_wedding)
             {
-                LogEvent("[MoreRain] There is no Alanis Morissetting here. Enjoy your wedding.");
+                LogEvent("There is no Alanis Morissetting here. Enjoy your wedding.");
                 return;
             }
-         
+
             Random rng = new Random(Guid.NewGuid().GetHashCode());
-            int genNumber = rng.Next(0, 100);
+            double genNumber = rng.NextDouble();
+
+           switch (Game1.currentSeason)
+            {
+                case "spring":
+                    if (genNumber < (ModConfig.spgBaseRainChance + (ModConfig.spgRainChanceIncrease * Game1.dayOfMonth)))
+                    {
+                        LogEvent("It is rainy outside.");
+                        Game1.weatherForTomorrow = Game1.weather_rain;
+                        genNumber = rng.NextDouble();
+                        if (genNumber < ModConfig.spgConvRainToStorm)
+                        {
+                            LogEvent("Well, it turns out it's quite stormy outside...");
+                            Game1.weatherForTomorrow = Game1.weather_lightning;
+                        }
+                    }
+
+                    if (genNumber < (ModConfig.spgBaseStormChance + (ModConfig.spgStormChanceIncrease * Game1.dayOfMonth)))
+                    {
+                        LogEvent("It is stormy outside.");
+                        Game1.weatherForTomorrow = Game1.weather_lightning;
+                        genNumber = rng.NextDouble();
+                    }
+
+                    if (genNumber < (ModConfig.spgBaseWindChance + (ModConfig.spgWindChanceIncrease * Game1.dayOfMonth)))
+                    {
+                        LogEvent("It is windy out. Hey, is that Dorothy?");
+                        Game1.weatherForTomorrow = Game1.weather_debris;
+                    }
+
+                    break;
+                case "summer":
+                    if (genNumber < (ModConfig.smrBaseRainChance + (ModConfig.smrRainChanceIncrease * Game1.dayOfMonth)))
+                    {
+                        LogEvent("It is rainy outside.");
+                        Game1.weatherForTomorrow = Game1.weather_rain;
+                        genNumber = rng.NextDouble();
+                        if (genNumber < ModConfig.smrConvRainToStorm)
+                        {
+                            LogEvent("Well, it turns out it's quite stormy outside...");
+                            Game1.weatherForTomorrow = Game1.weather_lightning;
+                        }
+                    }
 
 
+                    if (genNumber < (ModConfig.smrBaseStormChance + (ModConfig.smrStormChanceIncrease * Game1.dayOfMonth)))
+                    {
+                        LogEvent("It is stormy outside.");
+                        Game1.weatherForTomorrow = Game1.weather_lightning;
+                        genNumber = rng.NextDouble();
+                    }
+
+                    if (genNumber < (ModConfig.smrBaseWindChance + (ModConfig.smrWindChanceIncrease * Game1.dayOfMonth)))
+                    {
+                        LogEvent("It is windy out. Hey, is that Dorothy?");
+                        Game1.weatherForTomorrow = Game1.weather_debris;
+                    }
+
+                    break;
+                case "fall":
+                    if (genNumber < (ModConfig.falBaseRainChance + (ModConfig.falRainChanceIncrease * Game1.dayOfMonth)))
+                    {
+                        LogEvent("It is rainy outside.");
+                        Game1.weatherForTomorrow = Game1.weather_rain;
+                        genNumber = rng.NextDouble();
+                        if (genNumber < ModConfig.falConvRainToStorm)
+                        {
+                            LogEvent("Well, it turns out it's quite stormy outside...");
+                            Game1.weatherForTomorrow = Game1.weather_lightning;
+                        }
+                    }
+
+
+                    if (genNumber < (ModConfig.falBaseStormChance + (ModConfig.falStormChanceIncrease * Game1.dayOfMonth)))
+                    {
+                        LogEvent("It is stormy outside.");
+                        Game1.weatherForTomorrow = Game1.weather_lightning;
+                        genNumber = rng.NextDouble();
+                    }
+
+                    if (genNumber < (ModConfig.falBaseWindChance + (ModConfig.falWindChanceIncrease * Game1.dayOfMonth)))
+                    {
+                        LogEvent("It is windy out. Hey, is that Dorothy?");
+                        Game1.weatherForTomorrow = Game1.weather_debris;
+                    }
+
+                    if (Game1.dayOfMonth == 28)
+                        Game1.weatherForTomorrow = Game1.weather_snow; //it now snows on Fall 28.
+
+                    break;
+                case "winter":
+
+                    if (genNumber < (ModConfig.winBaseWindChance + (ModConfig.winWindChanceIncrease * Game1.dayOfMonth)))
+                    {
+                        LogEvent("It is windy out. Hey, is that Dorothy?");
+                        Game1.weatherForTomorrow = Game1.weather_debris;
+                    }
+
+                    if (genNumber < (ModConfig.winBaseSnowChance + (ModConfig.winSnowChanceIncrease * Game1.dayOfMonth)))
+                    {
+                        LogEvent("It is quite snowy out. Got hot chocolate?");
+                        Game1.weatherForTomorrow = Game1.weather_snow;
+                    }
+
+                    break;
+                default:
+                    Game1.weatherForTomorrow = Game1.weather_sunny; //fail safe.
+                    break;
+            }
+
+            //overrides
+            Game1.weatherForTomorrow = Game1.weather_debris;
+
+           
+            
 
         }
     }
