a29f7a9767e13078aadb01118c548eeb3c99d73b
Nicholas Johnson <koihimenakamura@gmail.com>
2016-10-14 17:51:59 -0700

    Initial Commit of this project
    Rechanging from the last project now.

---
+++ ClimateOfFerngill/ClimatesOfFerngill.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using StardewModdingAPI;
+using StardewValley;
+
+namespace ClimatesOfFerngill
+{
+    public class ClimatesOfFerngill : Mod
+    {
+        public ModConfig ModConfig { get; private set; } 
+        bool gameloaded;
+
+        public override void Entry(params object[] objects)
+        {
+          ModConfig = new ModConfig().InitializeConfig(BaseConfigPath);
+          StardewModdingAPI.Events.PlayerEvents.LoadedGame += PlayerEvents_LoadedGame;
+          StardewModdingAPI.Events.TimeEvents.DayOfMonthChanged += TimeEvents_DayOfMonthChanged;
+          Log.SyncColour("Climates of Ferngill rev20161014 by KoihimeNakamura but based off of Alpha_Omegasis's More Rain", ConsoleColor.Cyan);
+        }
+
+        private void LogEvent(string msg, bool logAllowed = false)
+        {
+            if (logAllowed) Log.Debug("[Climate] " + msg);
+        }
+
+        public void TimeEvents_DayOfMonthChanged(object sender, StardewModdingAPI.Events.EventArgsIntChanged e)
+        {
+            if (gameloaded == false) return;
+            UpdateWeather();
+        }
+
+        public void PlayerEvents_LoadedGame(object sender, StardewModdingAPI.Events.EventArgsLoadedGameChanged e)
+        {
+            gameloaded = true;
+            UpdateWeather();
+        }
+
+        void UpdateWeather(){
+            //sanity check - not entirely neccesary. 
+            if (Game1.weatherForTomorrow == Game1.weather_wedding)
+            {
+                if (!ModConfig.SuppressLog) Log.Info("[MoreRain] There is no Alanis Morissetting here. Enjoy your wedding.");
+                return;
+            }
+         
+            Random rng = new Random(Guid.NewGuid().GetHashCode());
+            int genNumber = rng.Next(0, 100);
+
+
+
+        }
+    }
+}
+
