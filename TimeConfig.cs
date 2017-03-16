11dbc1956cbdbe217b88d952f5b1f6c4acdc1809
Nicholas Johnson <johnson.nicholas.h@gmail.com>
2017-03-15 19:49:25 -0700

    Create TimeConfig.cs

---
+++ TimeConfig.cs
@@ -0,0 +1,12 @@
+namespace TimeReminder
+{
+    public class TimeConfig
+    {
+        public int NumOfMinutes { get; set;}
+
+        public TimeConfig()
+        {
+            NumOfMinutes = 6;
+        }
+    }
+}
