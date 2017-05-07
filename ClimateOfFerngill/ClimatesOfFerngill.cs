e19549cc843f743327bb23f821a1ec94f3094c31
Nicholas Johnson <koihimenakamura@gmail.com>
2017-05-06 22:31:56 -0700

    - Thanks to Pathos, blizzards are working! (#12)

--- ClimateOfFerngill/ClimatesOfFerngill.cs
+++ ClimateOfFerngill/ClimatesOfFerngill.cs
@@ -230,10 +230,10 @@ namespace ClimateOfFerngill
 
         private void DrawSnow()
         {
-            /* if (CurrWeather.IsBlizzard)
-            { */
-                //snowPos = Game1.updateFloatingObjectPositionForMovement(snowPos, new Vector2(Game1.viewport.X, Game1.viewport.Y),
-                //          Game1.previousViewportPosition, -1f);
+            if (CurrWeather.IsBlizzard)
+            { 
+                snowPos = Game1.updateFloatingObjectPositionForMovement(snowPos, new Vector2(Game1.viewport.X, Game1.viewport.Y),
+                          Game1.previousViewportPosition, -1f);
                 snowPos.X = snowPos.X % (16 * Game1.pixelZoom);
                 Vector2 position = new Vector2();
                 float num1 = -16 * Game1.pixelZoom + snowPos.X % (16 * Game1.pixelZoom);
@@ -246,14 +246,15 @@ namespace ClimateOfFerngill
                         position.Y = (int)num2;
                         Game1.spriteBatch.Draw(Game1.mouseCursors, position, new Microsoft.Xna.Framework.Rectangle?
                             (new Microsoft.Xna.Framework.Rectangle
-                            (245 + (int)(Game1.currentGameTime.TotalGameTime.TotalMilliseconds % 1200.0) / 75 * 16, 192, 16, 16)),
+                            (368 + (int)((Game1.currentGameTime.TotalGameTime.TotalMilliseconds + 150) % 1200.0) / 75 * 16, 192, 16, 16)),
                             Color.White * Game1.options.snowTransparency, 0.0f, Vector2.Zero,
                             Game1.pixelZoom + 1f / 1000f, SpriteEffects.None, 1f);
                         num2 += 16 * Game1.pixelZoom;
                     }
                     num1 += 16 * Game1.pixelZoom;
                 }
-            /* } */
+
+             }
         }
 
         private void DrawFog()
