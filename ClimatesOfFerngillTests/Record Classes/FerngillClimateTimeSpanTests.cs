fa66a42895b6abf3afd11276857c5975fed72e5b
Nicholas Johnson <johnson.nicholas.h@gmail.com>
2017-05-24 17:54:23 -0700

    more tests

---
+++ ClimatesOfFerngillTests/Record Classes/FerngillClimateTimeSpanTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClimateOfFerngill;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClimateOfFerngill.Tests
+{
+    [TestClass()]
+    public class FerngillClimateTimeSpanTests
+    {  
+        [TestMethod()]
+        public void RetrieveTempTest()
+        {
+            FerngillClimateTimeSpan Test = new FerngillClimateTimeSpan();
+            Test.AddWeatherChances
+        }
+    }
+}
