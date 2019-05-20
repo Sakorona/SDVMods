7bd2cb848b511c5254cc6c08828eff5178f4cc27
Nicholas Johnson <johnson.nicholas.h@gmail.com>
2019-05-19 17:35:45 -0700

    Update AnswerDialoguePatch.cs
    Some fixes, testing a change.

--- FerngillTaxes/Patches/AnswerDialoguePatch.cs
+++ FerngillTaxes/Patches/AnswerDialoguePatch.cs
@@ -10,15 +10,20 @@ namespace TwilightShards.FerngillTaxes.Patches
     static class AnswerDialoguePatch
     {
         static bool Prefix(bool __result)
-        {             
+        {           
+            double busCost = 500 * TwilightShards.FerngillTaxes.Options.SalesTax;
+            if (TwilightShards.FerngillTaxes.Options.TaxSubsiszedServices)
+                busCost = 50 ** TwilightShards.FerngillTaxes.Options.SalesTax;;
+            
             if (this.lastQuestionKey != null && this.afterQuestion == null)
             {
                 if (this.lastQuestionKey.Split(' ')[0] + "_" + answer.responseKey == "Bus_Yes")
                 {
                     NPC characterFromName = Game1.getCharacterFromName("Pam", false);
-                    if (Game1.player.Money >= (Game1.shippingTax ? 50 : 500) && this.characters.Contains(characterFromName) && characterFromName.getTileLocation().Equals(new Vector2(11f, 10f)))
+                    
+                    if (Game1.player.Money >= busCost && this.characters.Contains(characterFromName) && characterFromName.getTileLocation().Equals(new Vector2(11f, 10f)))
                     {
-                        Game1.player.Money -= Game1.shippingTax ? 50 : 500;
+                        Game1.player.Money -= busCost;
                         Game1.freezeControls = true;
                         Game1.viewportFreeze = true;
                         this.forceWarpTimer = 8000;
@@ -27,14 +32,14 @@ namespace TwilightShards.FerngillTaxes.Patches
                         if (Game1.player.mount != null)
                             Game1.player.mount.farmerPassesThrough = true;
                     }
-                    else if (Game1.player.Money < (Game1.shippingTax ? 50 : 500))
+                    else if (Game1.player.Money < busCost)
                         Game1.drawObjectDialogue(Game1.content.LoadString("Strings\\Locations:BusStop_NotEnoughMoneyForTicket"));
                     else
                         Game1.drawObjectDialogue(Game1.content.LoadString("Strings\\Locations:BusStop_NoDriver"));
-                    return true;
+                    __result = true;
                 }
             }
-            __result = base.answerDialogue(answer);
+            __result = __originalMethod.base.answerDialogue(answer);
             return false;
         }
     }
