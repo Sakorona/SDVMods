bba57f171a462ade3f0e1e93eb60da7a66f591c7
Sakorona <7285555+Sakorona@users.noreply.github.com>
2020-03-01 11:26:30 -0800

    Update IContentPatcherAPI.cs
    wrong API signature used.

--- LunarDisturbances/Integrations/IContentPatcherAPI.cs
+++ LunarDisturbances/Integrations/IContentPatcherAPI.cs
@@ -5,6 +5,6 @@ namespace TwilightShards.LunarDisturbances.Integrations
     /// <summary>The Content Patcher API which other mods can access.</summary>
     public interface IContentPatcherAPI
     {
-        void RegisterToken(IManifest mod, string name, object token);
+        void RegisterToken(IManifest mod, string name, Func<IEnumerable<string>> getValue);
     }
 }
